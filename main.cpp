71d695c416a2080e33cc19d2555dcda81c20c15a
Aven <a@avenf.com>
2026-03-27 14:37:12 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <GL/glut.h>
+#include <glm/glm.hpp>
+#include <iostream>
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+    glutInit(&argc, argv);
+    return 0;
+}
