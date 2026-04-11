0fb4149d4a18c5f9c1a548ea587d3b4f004a6c71
Aven <a@avenf.com>
2026-04-11 15:33:45 +0100

    cleaned up main.cpp into a class

--- main.cpp
+++ main.cpp
@@ -11,82 +11,89 @@
 
 class Application {
 public:
+    static inline float SCREEN_WIDTH = 1000.0;
+    static inline float SCREEN_HEIGHT = 800.0;
     static inline std::unique_ptr<Scene> scene = nullptr;
     static inline std::unique_ptr<InputManager> input_manager = nullptr;
+
+    static void init(int argc, char** argv, float screen_width, float screen_height) {
+        glutInit(&argc, argv);
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+        glutCreateWindow("Test");
+
+        glewExperimental = GL_TRUE;
+        if (const GLenum err = glewInit(); GLEW_OK != err) {
+            std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
+        }
+
+        glutDisplayFunc(render);
+
+        glutKeyboardFunc(handle_input_down);
+        glutKeyboardUpFunc(handle_input_up);
+
+        glutSpecialFunc(handle_input_down);
+        glutSpecialUpFunc(handle_input_up);
+
+        glutIdleFunc(update);
+
+        scene = std::make_unique<Scene>(screen_width, screen_height);
+        input_manager = std::make_unique<InputManager>(scene.get());
+    }
+    static void run() {
+        glutMainLoop();
+    }
+    static void update() {
+        if (scene && input_manager) {
+            scene->update();
+            input_manager->update();
+        }
+    }
+
+    static void render() {
+        if (scene) {
+            scene->render();
+        }
+    }
+
+    static void handle_input_down(unsigned char key, int x, int y) {
+        if (input_manager) {
+            input_manager->handle_input_down(key, x, y);
+        }
+    }
+
+    static void handle_input_down(int key, int x, int y) {
+        if (input_manager) {
+            input_manager->handle_input_down(key, x, y);
+        }
+    }
+
+    static void handle_input_up(unsigned char key, int x, int y) {
+        if (input_manager) {
+            input_manager->handle_input_up(key, x, y);
+        }
+    }
+
+    static void handle_input_up(int key, int x, int y) {
+        if (input_manager) {
+            input_manager->handle_input_up(key, x, y);
+        }
+    }
+
 };
 
-float SCREEN_WIDTH = 1000.0;
-float SCREEN_HEIGHT = 800.0;
-
-void render() {
-    if (Application::scene) {
-        Application::scene->render();
-    }
-}
-
-void handle_input_down(unsigned char key, int x, int y) {
-    if (Application::input_manager) {
-        Application::input_manager->handle_input_down(key, x, y);
-    }
-}
-
-void handle_input_down(int key, int x, int y) {
-    if (Application::input_manager) {
-        Application::input_manager->handle_input_down(key, x, y);
-    }
-}
-
-void handle_input_up(unsigned char key, int x, int y) {
-    if (Application::input_manager) {
-        Application::input_manager->handle_input_up(key, x, y);
-    }
-}
-
-void handle_input_up(int key, int x, int y) {
-    if (Application::input_manager) {
-        Application::input_manager->handle_input_up(key, x, y);
-    }
-}
-
-void update() {
-    if (Application::scene && Application::input_manager) {
-        Application::scene->update();
-        Application::input_manager->update();
-    }
-}
-
 int main(int argc, char** argv) {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutCreateWindow("Test");
 
-    glewExperimental = GL_TRUE;
-    if (const GLenum err = glewInit(); GLEW_OK != err) {
-        std::cerr << "Error: " << glewGetErrorString(err) << std::endl;
-    }
+    Application::init(argc, argv, Application::SCREEN_WIDTH, Application::SCREEN_HEIGHT);
 
     SceneObject::add_shaders({
         {"BasicView", "glslfiles/basicTransformations.vert", "glslfiles/basicTransformations.frag"},
         {"Cube", "glslfiles/basic.vert", "glslfiles/basic.frag"}
     });
 
-    Application::scene = std::make_unique<Scene>(SCREEN_WIDTH, SCREEN_HEIGHT);
-    Application::input_manager = std::make_unique<InputManager>(Application::scene.get());
-
     Application::scene->addObject(std::make_unique<Cube>(glm::vec3(0, 0, -5), 2.0f));
     Application::scene->addObject(std::make_unique<ModelObject>("TestModels/axes.obj", "BasicView",glm::vec3(0, 5, 5)));
 
-    glutDisplayFunc(render);
-
-    glutKeyboardFunc(handle_input_down);
-    glutKeyboardUpFunc(handle_input_up);
-
-    glutSpecialFunc(handle_input_down);
-    glutSpecialUpFunc(handle_input_up);
-
-    glutIdleFunc(update);
-
-    glutMainLoop();
+    Application::run();
 
     return 0;
 }
